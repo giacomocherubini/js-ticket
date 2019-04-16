d9ebdfb6f311b6a331016fd4de16845f3d322339
giacomocherubini <giacomo.cherubini1@gmail.com>
2019-04-16 17:27:21 +0200

    fine

---
+++ main.js
@@ -0,0 +1,17 @@
+var chilometri = prompt('quanti chilometri?');
+var eta = prompt('quanti anni hai?');
+var prezzo_chilometro;
+prezzo_chilometro = 0.21;
+var prezzo_biglietto = chilometri * prezzo_chilometro;
+
+if (eta < 18)
+{alert (prezzo_biglietto / 100 * 80 );
+}
+
+if (eta > 65)
+{alert (prezzo_biglietto /100 * 60);
+}
+
+else
+{alert (prezzo_biglietto);
+}
